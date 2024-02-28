e4e0346acaa61e052edb7e2b5687f6f95f0e5c04
Koen Niemeijer <koen.niemeijer@kuleuven.be>
2024-02-28 15:21:54 +0100

    Further package initialization

---
+++ R/read_mpath.R
@@ -0,0 +1,21 @@
+#' Read an m-Path file into a tibble
+#'
+#' @description `r lifecycle::badge("experimental")`
+#' This function reads an m-Path file into a tibble.
+#'
+#' @details
+#' Some more details...
+#'
+#' @param file A string with the path to the m-Path file
+#'
+#' @return A \link[tibble]{tibble} with the contents of the m-Path file.
+#' @export
+#'
+#' @examples
+#' # TODO: create working examples
+#' read_mpath("path/to/file")
+read_mpath <- function(
+    file
+) {
+  file
+}
