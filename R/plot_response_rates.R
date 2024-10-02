7217a549f69d2c08e36c11e57d1bea7c2bca5f0d
Koen Niemeijer <u0134047@kuleuven.be>
2024-10-02 17:12:01 +0200

    Remove deprecated aes_string() from plot_response_rates()

--- R/plot_response_rates.R
+++ R/plot_response_rates.R
@@ -19,9 +19,9 @@ response_rate_per_day <- function(
 
 #' Plots response rate per day (and per participant)
 #'
-#' @description
-#' This function returns a ggplot object with the response rate per day (x axis) and participant (color).
-#' Note that instead of using calendar dates, the function returns a plot grouped by the day inside the study for the participant.
+#' @description This function returns a ggplot object with the response rate per day (x axis) and
+#' participant (color). Note that instead of using calendar dates, the function returns a plot
+#' grouped by the day inside the study for the participant.
 #'
 #' @param data data frame with data
 #' @param valid_col name of the column that stores whether the beep was answered or not
@@ -40,44 +40,53 @@ response_rate_per_day <- function(
 #' time_col = sent,
 #' participant_col = participant,
 #' valid_col = answered)
-#' # The resulting ggplot object can be formatted using ggplot2 functions (see ggplot2 documentation).
+#' # The resulting ggplot object can be formatted using ggplot2 functions (see ggplot2
+#' # documentation).
 #'
-plot_response_rates <- function(data,
-                                valid_col,
-                                participant_col,  # specify participant variable
-                                time_col # specify time variable
+plot_response_rates <- function(
+    data,
+    valid_col,
+    participant_col,  # specify participant variable
+    time_col # specify time variable
 ){
-  time_col <- enquo(time_col)
-  participant_col <- enquo(participant_col)
-  valid_col <- enquo(valid_col)
 
   data_plot <- response_rate_per_day(
     data = data,
-    valid_col = !!valid_col,
-    participant_col = !!participant_col,
-    time_col = !!time_col
+    valid_col = {{ valid_col }},
+    participant_col = {{ participant_col }},
+    time_col = {{ time_col }}
   )
 
   # turn participant into a factor for plotting purposes
-  data_plot <- data_plot %>%
-    mutate(participant = as.factor(!!participant_col)) # make participant_col a factor and assign to 'participant' (for plotting purposes)
+  data_plot <- data_plot |>
+    mutate(participant = as.factor({{ participant_col }}))
 
   # get n of participants for plotting purposes
-  num_unique <- data_plot %>%
-    pull(!!participant_col) %>%
-    unique() %>% length()
+  num_unique <- data_plot |>
+    ungroup() |>
+    summarise(unique = dplyr::n_distinct({{ participant_col }})) |>
+    pull("unique")
 
-  ggplot(data_plot, aes_string(x = 'day',
-                        y = 'response_rate',
-                        group = 'participant',
-                        color = 'participant',
-                        shape = 'participant',
-                        linetype = 'participant')) +
+  data_plot |>
+    ggplot(
+      aes(
+        x = .data$day,
+        y = .data$response_rate,
+        group = .data$participant,
+        color = .data$participant,
+        shape = .data$participant,
+        linetype = .data$participant
+      )) +
     ggplot2::geom_line() +
     ggplot2::geom_point() +
-    ggplot2::scale_linetype_manual(values = rep(c("solid", "dashed", "dotted"), length.out = num_unique)) +
+    ggplot2::scale_linetype_manual(values = rep(
+      c("solid", "dashed", "dotted"),
+      length.out = num_unique
+    )) +
     ggplot2::scale_shape_manual(values = rep(c(3, 8, 15, 16, 17, 18), length.out = num_unique)) +
-    ggplot2::labs(title = "Response rate per day",
-         x = "Date",
-         y = "Response rate")
+    ggplot2::labs(
+      title = "Response rate per day",
+      x = "Date",
+      y = "Response rate"
+    )
 }
